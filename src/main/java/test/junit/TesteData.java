355b33f53735d96269985c805a482f841c833802
Pelegrino <pelegrino@gmail.com>
2023-01-27 14:58:58 -0300

    Criando a classe ReportUtil

---
+++ src/main/java/test/junit/TesteData.java
@@ -0,0 +1,16 @@
+package test.junit;
+
+import org.junit.Test;
+
+import jsfprimefaces.report.util.DateUtils;
+
+public class TesteData {
+
+	@Test
+	public void test() {
+		
+		System.out.println(DateUtils.getDateAtualReportName());
+		
+	}
+
+}
