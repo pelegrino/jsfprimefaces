ea0fb80f326f86e49a3df98e5c0ecb57c5a236e4
Pelegrino <pelegrino@gmail.com>
2023-01-30 09:19:04 -0300

    Criando o Filter FilterOpenSessionInView

---
+++ src/main/java/jsfprimefaces/filter/FilterOpenSessionInView.java
@@ -0,0 +1,114 @@
+package jsfprimefaces.filter;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.web.filter.DelegatingFilterProxy;
+
+import jsfprimefaces.hibernate.session.HibernateUtil;
+import jsfprimefaces.listener.ContextLoaderListenerCaixakiUtils;
+import jsfprimefaces.model.classes.Entidade;
+import jsfprimefaces.utils.UtilFramework;
+
+@WebFilter(filterName = "conexaoFiltro")
+public class FilterOpenSessionInView extends DelegatingFilterProxy implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	
+	private static SessionFactory sf;
+	
+	//Executa uma vez
+	//Executa a aplicação quando o servidor sobe
+	@Override
+	protected void initFilterBean() throws ServletException {
+		sf = HibernateUtil.getSessionFactory();
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws ServletException, IOException {
+		
+		//JDBC Spring
+		BasicDataSource springDataSource = (BasicDataSource) ContextLoaderListenerCaixakiUtils.getBean("springDataSource");
+		DefaultTransactionDefinition def = new DefaultTransactionDefinition();
+		PlatformTransactionManager transactionManager = new DataSourceTransactionManager(springDataSource);
+		TransactionStatus status = transactionManager.getTransaction(def);
+		
+		try {
+			
+			//Define a codificação
+			request.setCharacterEncoding("UTF-8");
+			
+			//Captura o que o usuário faz
+			HttpServletRequest request2 = (HttpServletRequest) request;
+			HttpSession sessao = request2.getSession();
+			Entidade userLogadoSessao = (Entidade) sessao.getAttribute("userLogadoSessao");
+			
+			if (userLogadoSessao != null) {
+				UtilFramework.getThreadLocal().set(userLogadoSessao.getEnt_codigo());
+			}
+			
+			sf.getCurrentSession().beginTransaction();
+				
+			//Antes de executar a ação (request)
+			chain.doFilter(request, response);
+			
+			//Após execução
+			transactionManager.commit(status);
+			
+			if (sf.getCurrentSession().getTransaction().isActive()) {
+				sf.getCurrentSession().flush();
+				sf.getCurrentSession().getTransaction().commit();
+			}
+			
+			if (sf.getCurrentSession().isOpen()) {
+				sf.getCurrentSession().close();
+			}
+			
+			response.setCharacterEncoding("UTF-8");
+			response.setContentType("text/html; charset=UTF-8");
+			
+		} catch (Exception e) {
+			transactionManager.rollback(status);
+			e.printStackTrace();
+				
+			if (sf.getCurrentSession().getTransaction().isActive()) {
+				sf.getCurrentSession().getTransaction().rollback();
+			}
+			
+			if (sf.getCurrentSession().isOpen()) {
+				sf.getCurrentSession().close();
+			}
+			
+		} finally {
+			if (sf.getCurrentSession().isOpen()) {
+				if (sf.getCurrentSession().beginTransaction().isActive()) {
+					sf.getCurrentSession().flush();
+					sf.getCurrentSession().clear();
+					
+				}
+				
+				if (sf.getCurrentSession().isOpen()) {
+					sf.getCurrentSession().close();
+				}
+			}
+		}
+		
+	}
+	
+}
